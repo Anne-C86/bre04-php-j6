38cd23735a68dbebb9c224797a7bbd95d8d95e6d
Anne-C86 <anne-charlotte.lucas@3wa.io>
2025-06-11 11:01:14 +0200

    etape 1 : connexion

---
+++ connexion.php
@@ -0,0 +1,15 @@
+<?php
+$host = "db.3wa.io";
+$port = "3306";
+$dbname = "annecharlottelucas_phpj6";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+$user = "annecharlottelucas";
+$password = "08c91f0ff690677f4e839d460d76d0a4";
+
+$db = new PDO(
+        $connexionString,
+        $user,
+        $password
+);
+
+var_dump($db);
